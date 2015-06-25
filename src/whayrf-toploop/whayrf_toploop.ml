8838a14184b5e12e9bbec253bdf9852d55467dfc
Leandro Facchinetti <me@leafac.com>
2015-06-25 14:25:05 -0400

    Add support for typechecking in toploop

--- src/whayrf-toploop/whayrf_toploop.ml
+++ src/whayrf-toploop/whayrf_toploop.ml
@@ -3,14 +3,20 @@ open Batteries;;
 open Whayrf_ast_pretty;;
 open Whayrf_ast_wellformedness;;
 open Whayrf_interpreter;;
+open Whayrf_typechecker;;
 
 let toploop_operate e =
   print_string "\n";
   begin
     try
       check_wellformed_expr e;
-      let v,env = eval e in
-      print_string (pretty_var v ^ " where "  ^ pretty_env env ^ "\n");
+      (
+        if typecheck e then
+         let v,env = eval e in
+         print_string (pretty_var v ^ " where "  ^ pretty_env env ^ "\n");
+        else
+          print_string "Type error.\n"
+      )
     with
       | Illformedness_found(ills) ->
           print_string "Provided expression is ill-formed:\n";
