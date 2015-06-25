e8b02070841a340a5562f82f08a4bc395dce40c0
Leandro Facchinetti <me@leafac.com>
2015-06-25 11:27:52 -0400

    Add typechecking façade

---
+++ src/whayrf-typechecker/whayrf_typechecker.ml
@@ -0,0 +1,46 @@
+(**
+  This module represents the entry point for the Whayrf typechecking process.
+*)
+
+open Batteries;;
+open Printf;;
+
+open Whayrf_ast_pretty;;
+open Whayrf_constraint_closure;;
+open Whayrf_initial_alignment;;
+open Whayrf_types;;
+open Whayrf_types_pretty;;
+
+(* ************************************************************************** *)
+(* LOGGER *)
+
+let logger = Whayrf_logger.make_logger "Whayrf_typechecker";;
+
+(* ************************************************************************** *)
+(* TYPECHECKING *)
+
+(**
+  Performs typechecking of the provided expression.
+  @param e The expression to typecheck.
+  @return [true] if the expression typechecks; [false] if it does not.
+*)
+let typecheck e =
+  (* Step 1: Initially align the expression. *)
+  let Constrained_type (_, constraint_set) = initial_align_expr e in
+  logger `trace
+    (sprintf
+      "Initial alignment of %s yields constraints %s"
+      (pretty_expr e) (pretty_constraint_set constraint_set)
+    )
+  ;
+  (* Step 2: Perform constraint closure. *)
+  let constraint_set' = perform_closure constraint_set in
+  logger `trace
+    (sprintf
+      "Constraint closure yields constraints %s"
+      (pretty_constraint_set constraint_set')
+    )
+  ;
+  (* Step 3: Look for inconsistencies. *)
+  is_consistent constraint_set'
+  ;;
