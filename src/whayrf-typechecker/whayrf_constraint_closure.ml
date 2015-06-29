8cbac9b1cd0574cb835ac837abcc94290f247bdf
Leandro Facchinetti <me@leafac.com>
2015-06-29 13:51:24 -0400

    Build dispatch table

--- src/whayrf-typechecker/whayrf_constraint_closure.ml
+++ src/whayrf-typechecker/whayrf_constraint_closure.ml
@@ -1,6 +1,7 @@
 open Batteries;;
 
 open Whayrf_ast;;
+open Whayrf_initial_alignment;;
 open Whayrf_types;;
 open Whayrf_utils;;
 
@@ -1260,8 +1261,111 @@ let rec perform_closure constraint_set =
     augmented_constraint_set
 ;;
 
+let rec unfreshen_function_value (Function_value (parameter, body)) =
+  Function_value (
+    unfreshen_var parameter,
+    unfreshen_expr body
+  )
+
+and unfreshen_var (Var (name, _)) =
+  (* TODO: Confirm that None is the best option here (as opposed to an empty
+     freshening stack). *)
+  Var (name, None)
+
+and unfreshen_expr (Expr(clauses)) =
+  Expr (
+    List.map unfreshen_clause clauses
+  )
+
+and unfreshen_clause (Clause (var, clause_body)) =
+  Clause (
+    unfreshen_var var,
+    unfreshen_clause_body clause_body
+  )
+
+and unfreshen_clause_body clause_body =
+  match clause_body with
+  | Value_body (value) -> Value_body (unfreshen_value value)
+  | Var_body (var) -> Var_body (unfreshen_var var)
+  | Appl_body (function_var, parameter_var) ->
+    Appl_body (unfreshen_var function_var, unfreshen_var parameter_var)
+  | Projection_body (record_var, label) ->
+    Projection_body (unfreshen_var record_var, label)
+  | Conditional_body (subject_var, pattern, function_then_value, function_else_value) ->
+    Conditional_body (
+      unfreshen_var subject_var,
+      pattern,
+      unfreshen_function_value function_then_value,
+      unfreshen_function_value function_else_value
+    )
+
+and unfreshen_value value =
+  match value with
+  | Value_record (record_value) -> Value_record (unfreshen_record_value record_value)
+  | Value_function (function_value) -> Value_function (unfreshen_function_value function_value)
+
+and unfreshen_record_value (Record_value (record_elements)) =
+  Record_value (
+    Ident_map.map unfreshen_var record_elements
+  )
+;;
+
 let build_dispatch_table constraint_set =
-  fun function_value pattern ->
-    (* TODO: Not implemented yet. *)
-    false
+  fun value pattern ->
+    match value with
+    | Value_function (function_value) ->
+      let unfreshened_function_value =
+        unfreshen_function_value function_value
+      in
+      let function_type =
+        initial_align_function unfreshened_function_value
+      in
+      let is_antimatch =
+        not (
+          constraint_set
+          |> Constraint_set.enum
+          |> Enum.exists
+            (
+              fun tconstraint ->
+                match tconstraint with
+                | Function_pattern_matching_constraint (
+                    Function_pattern_matching_constraint_negative (
+                      other_function_type,
+                      other_pattern
+                    )
+                  ) ->
+                  (function_type = other_function_type) &&
+                  (pattern = other_pattern)
+                | _ -> false
+            )
+        )
+      in
+      if is_antimatch then
+        false
+      else
+        let is_match =
+          constraint_set
+          |> Constraint_set.enum
+          |> Enum.exists
+            (
+              fun tconstraint ->
+                match tconstraint with
+                | Function_pattern_matching_constraint (
+                    Function_pattern_matching_constraint_positive (
+                      other_function_type,
+                      other_pattern
+                    )
+                  ) ->
+                  (function_type = other_function_type) &&
+                  (pattern = other_pattern)
+                | _ -> false
+            )
+        in
+        if is_match then
+          true
+        else
+          raise (Invariant_failure "Constraint absent from negative pattern set should be present in positive pattern set.")
+    | _ ->
+      (* TODO: Not implemented yet. *)
+      false
 ;;
