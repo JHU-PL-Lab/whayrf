670eda0b085595c99ab44c5c95292ec1bb82f274
Leandro Facchinetti <me@leafac.com>
2015-06-29 10:11:16 -0400

    Add Function Closure from Constraint Closure rules

--- src/whayrf-typechecker/whayrf_constraint_closure.ml
+++ src/whayrf-typechecker/whayrf_constraint_closure.ml
@@ -1169,3 +1169,25 @@ and function_pattern_search_type_variable type_variable constraint_set pattern =
     )
   |> Enum.reduce Constraint_set.union
 ;;
+
+let perform_function_closure constraint_set =
+  constraint_set
+  |> Constraint_set.enum
+  |> Enum.filter_map
+    (
+      fun tconstraint ->
+        match tconstraint with
+        | Lower_bound_constraint (
+            Conditional_lower_bound (
+              subject_type_variable,
+              pattern,
+              _,
+              _
+            ),
+            _
+          ) ->
+          Some (function_pattern_search_type_variable subject_type_variable constraint_set pattern)
+        | _ -> None
+    )
+  |> Enum.reduce Constraint_set.union
+;;
