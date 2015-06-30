080ae4a36b04cdffd003610dd12492a4631ab706
Leandro Facchinetti <me@leafac.com>
2015-06-30 14:50:02 -0400

    Extract notation file

--- src/whayrf-typechecker/whayrf_constraint_closure.ml
+++ src/whayrf-typechecker/whayrf_constraint_closure.ml
@@ -2,25 +2,10 @@ open Batteries;;
 
 open Whayrf_ast;;
 open Whayrf_initial_alignment;;
+open Whayrf_notation;;
 open Whayrf_types;;
 open Whayrf_types_pretty;;
 open Whayrf_utils;;
-
-let project_pattern_set label pattern_set =
-  pattern_set
-  |> Pattern_set.enum
-  |> Enum.filter_map (
-    fun pattern ->
-      match pattern with
-      | Record_pattern (pattern_elements) ->
-        if Ident_map.mem label pattern_elements then
-          Some (Ident_map.find label pattern_elements)
-        else
-          None
-      | _ -> raise @@ Invariant_failure "Tried to project label out of a non-record pattern."
-  )
-  |> Pattern_set.of_enum
-;;
 
 let rec rename_pattern_variable pattern new_pattern_variable old_pattern_variable =
   match pattern with
---
+++ src/whayrf-typechecker/whayrf_notation.ml
@@ -0,0 +1,27 @@
+open Batteries;;
+
+open Whayrf_ast;;
+open Whayrf_initial_alignment;;
+open Whayrf_types;;
+open Whayrf_types_pretty;;
+open Whayrf_utils;;
+
+(** Implements the Notation part that allows us to "project labels out of
+    pattern sets".
+*)
+let project_pattern_set label pattern_set =
+  pattern_set
+  |> Pattern_set.enum
+  |> Enum.filter_map (
+    fun pattern ->
+      match pattern with
+      | Record_pattern (pattern_elements) ->
+        if Ident_map.mem label pattern_elements then
+          Some (Ident_map.find label pattern_elements)
+        else
+          (* QUESTION: Should we raise here? If we do, tests don't pass anymore. *)
+          None
+      | _ -> raise @@ Invariant_failure "Tried to project label out of a non-record pattern."
+  )
+  |> Pattern_set.of_enum
+;;
