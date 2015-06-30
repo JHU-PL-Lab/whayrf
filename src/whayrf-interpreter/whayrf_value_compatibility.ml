f9b71e1a005891eacc157b00f1ed67c947b333c1
Leandro Facchinetti <me@leafac.com>
2015-06-30 14:23:10 -0400

    Refactor Value Compatibility

--- src/whayrf-interpreter/whayrf_value_compatibility.ml
+++ src/whayrf-interpreter/whayrf_value_compatibility.ml
@@ -6,30 +6,53 @@ open Whayrf_ast_uid;;
 open Whayrf_environment;;
 open Whayrf_evaluation_failure;;
 
-let rec is_compatible value env pattern dispatch_table =
-  match pattern with
-  | Record_pattern(js) ->
-    begin
-      match value with
-      | Value_record(Record_value(is)) ->
-        Ident_map.fold (
-          fun label pattern' result ->
-            result &&
-            Ident_map.mem label is &&
-            let value' = lookup env @@ Ident_map.find label is in
-            is_compatible value' env pattern' dispatch_table
-        ) js true
-      | Value_function(_) -> false
-    end
+(**
+   Value Compatibility.
+*)
+let rec is_compatible value environment pattern dispatch_table =
+  (* LOOKUP is already performed when we get to this rule, since the parameter
+     is already a value. This is made to have a sane API to `is_compatible'
+     since values and variables are of different types. *)
+  match (value, pattern) with
+  (* RECORD *)
+  | (
+    Value_record(Record_value(record_elements)),
+    Record_pattern(pattern_elements)
+  ) ->
+    pattern_elements
+    |> Ident_map.enum
+    |> Enum.for_all
+      (
+        fun (pattern_label, pattern_subpattern) ->
+          record_elements
+          |> Ident_map.enum
+          |> Enum.exists
+            (
+              fun (record_label, record_subvariable) ->
+                (pattern_label = record_label) &&
+                (
+                  (* Here is an example of where the LOOKUP rule is performed. *)
+                  let record_subvalue = lookup environment record_subvariable in
+                  is_compatible
+                    record_subvalue
+                    environment
+                    pattern_subpattern
+                    dispatch_table
+                )
+            )
+      )
 
-  | Function_pattern(parameter_pattern, body_pattern) ->
-    begin
-      match value with
-      | Value_record(_) -> false
-      | Value_function(_) -> dispatch_table value pattern
-    end
+  (* FUNCTION *)
+  | (Value_function(_), Function_pattern(_))
 
-  | Pattern_variable_pattern(_)
-  | Forall_pattern(_) ->
+  (* FORALL *)
+  | (_, Forall_pattern(_))
+
+  (* EXISTS *)
+  | (_, Pattern_variable_pattern(_)) ->
     dispatch_table value pattern
+
+  (* Fallback *)
+  | _ ->
+    false
 ;;
