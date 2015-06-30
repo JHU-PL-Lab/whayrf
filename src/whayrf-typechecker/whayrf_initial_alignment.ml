0b783e4331857a0cd469959f3b687602eb8abccd
Leandro Facchinetti <me@leafac.com>
2015-06-30 15:08:05 -0400

    Refactor alignment
    Make it align better with the Figure :)

--- src/whayrf-typechecker/whayrf_initial_alignment.ml
+++ src/whayrf-typechecker/whayrf_initial_alignment.ml
@@ -3,6 +3,14 @@ open Batteries;;
 open Whayrf_ast;;
 open Whayrf_types;;
 
+(** Alignment is the operation that, given a value, figures out an initial
+    constrained type for it. This constrained type can later be enhanced by
+    performing constraint closure on its constraint set.
+*)
+
+(** Perform initial alignment on expressions (lists of clauses) (first section
+    of diagram).
+*)
 let rec initial_align_expr (Expr(clauses)) =
   clauses
   |> List.map initial_align_clause
@@ -14,10 +22,12 @@ let rec initial_align_expr (Expr(clauses)) =
       )
   )
 
+(** Perform initial alignment on clauses (second section of diagram). *)
 and initial_align_clause (Clause (Var(left_ident, _), body)) =
   let type_variable = Type_variable (left_ident) in
   match body with
-  | Value_body (value) ->
+  (* RECORD *)
+  | Value_body (Value_record(Record_value(record_elements))) ->
     Constrained_type (
       type_variable,
       Constraint_set.add
@@ -25,7 +35,14 @@ and initial_align_clause (Clause (Var(left_ident, _), body)) =
           Lower_bound_constraint (
             Restricted_type_lower_bound (
               Restricted_type (
-                initial_align_value value,
+                Record_type (
+                  Ident_map.map
+                    (
+                      fun (Var(ident, _)) ->
+                        Type_variable (ident)
+                    )
+                    record_elements
+                ),
                 Type_restriction (
                   Positive_pattern_set(Pattern_set.empty),
                   Negative_pattern_set(Pattern_set.empty)
@@ -38,14 +55,23 @@ and initial_align_clause (Clause (Var(left_ident, _), body)) =
         Constraint_set.empty
     )
 
-  | Var_body (Var(right_ident, _)) ->
+  (* FUNCTION *)
+  | Value_body (Value_function(function_value)) ->
     Constrained_type (
       type_variable,
       Constraint_set.add
         (
           Lower_bound_constraint (
-            Type_variable_lower_bound (
-              Type_variable (right_ident)
+            Restricted_type_lower_bound (
+              Restricted_type (
+                Function_type_type (
+                  initial_align_function function_value
+                ),
+                Type_restriction (
+                  Positive_pattern_set(Pattern_set.empty),
+                  Negative_pattern_set(Pattern_set.empty)
+                )
+              )
             ),
             type_variable
           )
@@ -53,22 +79,7 @@ and initial_align_clause (Clause (Var(left_ident, _), body)) =
         Constraint_set.empty
     )
 
-  | Appl_body (Var(function_ident, _), Var(parameter_ident, _)) ->
-    Constrained_type (
-      type_variable,
-      Constraint_set.add
-        (
-          Lower_bound_constraint (
-            Application_lower_bound (
-              Type_variable (function_ident),
-              Type_variable (parameter_ident)
-            ),
-            type_variable
-          )
-        )
-        Constraint_set.empty
-    )
-
+  (* PROJECTION *)
   | Projection_body (Var(record_ident, _), label) ->
     Constrained_type (
       type_variable,
@@ -85,6 +96,40 @@ and initial_align_clause (Clause (Var(left_ident, _), body)) =
         Constraint_set.empty
     )
 
+  (* VARIABLE *)
+  | Var_body (Var(right_ident, _)) ->
+    Constrained_type (
+      type_variable,
+      Constraint_set.add
+        (
+          Lower_bound_constraint (
+            Type_variable_lower_bound (
+              Type_variable (right_ident)
+            ),
+            type_variable
+          )
+        )
+        Constraint_set.empty
+    )
+
+  (* APPLICATION *)
+  | Appl_body (Var(function_ident, _), Var(parameter_ident, _)) ->
+    Constrained_type (
+      type_variable,
+      Constraint_set.add
+        (
+          Lower_bound_constraint (
+            Application_lower_bound (
+              Type_variable (function_ident),
+              Type_variable (parameter_ident)
+            ),
+            type_variable
+          )
+        )
+        Constraint_set.empty
+    )
+
+  (* CONDITIONAL *)
   | Conditional_body (Var (subject_ident, _), pattern, function_value_match, function_value_doesnt_match) ->
     Constrained_type (
       type_variable,
@@ -103,25 +148,7 @@ and initial_align_clause (Clause (Var(left_ident, _), body)) =
         Constraint_set.empty
     )
 
-and initial_align_value value =
-  match value with
-  | Value_record(record_value) ->
-    Record_type (initial_align_record record_value)
-  | Value_function(function_value) ->
-    Function_type_type (initial_align_function function_value)
-
-and initial_align_record (Record_value (records)) =
-  Ident_map.fold
-    (
-      fun label (Var(ident, _)) initial_aligned_records ->
-        Ident_map.add
-          label
-          (Type_variable (ident))
-          initial_aligned_records
-    )
-    records
-    Ident_map.empty
-
+(** Perform initial alignment on function values (third section of diagram). *)
 and initial_align_function (Function_value (Var(parameter_ident, _), body)) =
   Function_type (
     (Type_variable parameter_ident),
