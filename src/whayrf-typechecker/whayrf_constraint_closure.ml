6fdfce9ffc7a736abd05bf88b5c6f44338787e0b
Leandro Facchinetti <me@leafac.com>
2015-06-30 16:29:09 -0400

    Extract Function Constraint Closure

--- src/whayrf-typechecker/whayrf_constraint_closure.ml
+++ src/whayrf-typechecker/whayrf_constraint_closure.ml
@@ -2,6 +2,7 @@ open Batteries;;
 
 open Whayrf_ast;;
 open Whayrf_consistency;;
+open Whayrf_constraint_closure_function;;
 open Whayrf_constraint_closure_non_function;;
 open Whayrf_function_pattern_search;;
 open Whayrf_initial_alignment;;
@@ -11,28 +12,6 @@ open Whayrf_type_compatibility;;
 open Whayrf_types;;
 open Whayrf_types_pretty;;
 open Whayrf_utils;;
-
-let perform_function_closure constraint_set =
-  constraint_set
-  |> Constraint_set.enum
-  |> Enum.filter_map
-    (
-      fun tconstraint ->
-        match tconstraint with
-        | Lower_bound_constraint (
-            Conditional_lower_bound (
-              subject_type_variable,
-              pattern,
-              _,
-              _
-            ),
-            _
-          ) ->
-          Some (function_pattern_search_type_variable subject_type_variable constraint_set pattern)
-        | _ -> None
-    )
-  |> Enum.fold Constraint_set.union Constraint_set.empty
-;;
 
 let rec perform_closure constraint_set =
   let closure_functions =
