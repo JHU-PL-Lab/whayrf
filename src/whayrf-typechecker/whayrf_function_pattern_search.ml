d0b04a19883f297d8e0b422f1285d5dbf5dcd91b
Leandro Facchinetti <me@leafac.com>
2015-07-07 11:19:33 -0400

    Add function to find function pattern matching cases

--- src/whayrf-typechecker/whayrf_function_pattern_search.ml
+++ src/whayrf-typechecker/whayrf_function_pattern_search.ml
@@ -275,3 +275,120 @@ and function_pattern_search_type_variable perform_closure type_variable constrai
     )
   |> Enum.fold Constraint_set.union Constraint_set.empty
 ;;
+
+
+
+
+
+
+
+
+
+type function_pattern_matching_case = Function_pattern_matching_case of function_type * pattern;;
+
+module Function_pattern_matching_case_order =
+struct
+  type t = function_pattern_matching_case
+  let compare = compare
+end
+;;
+module Function_pattern_matching_case_set = Set.Make(Function_pattern_matching_case_order);;
+
+
+(** Find Function Matching looks for all pairs of function types and patterns
+    that can be explored by the Function Pattern Search. It comes in three
+    flavors, one that takes a raw type (ttype), one that takes a restricted type
+    and another that takes a type variable.
+
+    It's inspired by Function Pattern Search. *)
+let rec find_function_pattern_matching_cases_ttype ttype constraint_set pattern =
+  match (ttype, pattern) with
+  (* FUNCTION MATCH and FUNCTION ANTI-MATCH *)
+  (* These rules are almost identical, so they share the code. *)
+  | (
+    Function_type_type (
+      function_type
+    ),
+    Function_pattern (
+      _
+    )
+  )
+
+  (* FORALL *)
+  | (
+    Function_type_type (
+      function_type
+    ),
+    Forall_pattern (
+      _
+    )
+  ) ->
+    Function_pattern_matching_case_set.singleton
+      (Function_pattern_matching_case (function_type, pattern))
+
+  (* RECORD *)
+  (* Align the record type with the record pattern and recurse on the type
+     variable under the record label. This implementation doesn't apply the rule
+     once, but performs the fixpoint and returns the resulting set. *)
+  | (
+    Record_type (record_elements),
+    Record_pattern (pattern_elements)
+  ) ->
+    record_elements
+    |> Ident_map.enum
+    |> Enum.map
+      (
+        fun (record_label, type_variable) ->
+          pattern_elements
+          |> Ident_map.enum
+          |> Enum.filter_map
+            (
+              fun (pattern_label, pattern) ->
+                if record_label = pattern_label then
+                  Some (
+                    find_function_pattern_matching_cases_type_variable
+                      type_variable
+                      constraint_set
+                      pattern
+                  )
+                else
+                  None
+            )
+          |> Enum.fold Function_pattern_matching_case_set.union Function_pattern_matching_case_set.empty
+      )
+    |> Enum.fold Function_pattern_matching_case_set.union Function_pattern_matching_case_set.empty
+
+  | _ ->
+    Function_pattern_matching_case_set.empty
+
+(** FILTERED TYPE *)
+(* Simply ignore the filter. *)
+and find_function_pattern_matching_cases_restricted_type
+    (Restricted_type (ttype, _))
+    constraint_set
+    pattern =
+  find_function_pattern_matching_cases_ttype ttype constraint_set pattern
+
+(** TYPE SELECTION *)
+(* This implementation doesn't apply the rule once, but performs the fixpoint
+   and returns the resulting set. *)
+and find_function_pattern_matching_cases_type_variable type_variable constraint_set pattern =
+  logger `trace ("Looking for type variable `" ^ pretty_type_variable type_variable ^ "' in the constraint set: `"  ^ pretty_constraint_set constraint_set ^ "', and matching it with pattern `" ^ pretty_pattern pattern ^ "'.");
+  constraint_set
+  |> Constraint_set.enum
+  |> Enum.filter_map
+    (
+      fun tconstraint ->
+        match tconstraint with
+        | Lower_bound_constraint (
+            Restricted_type_lower_bound (restricted_type),
+            other_type_variable
+          ) ->
+          if type_variable = other_type_variable then
+            Some (find_function_pattern_matching_cases_restricted_type restricted_type constraint_set pattern)
+          else
+            None
+        | _ -> None
+    )
+  |> Enum.fold Function_pattern_matching_case_set.union Function_pattern_matching_case_set.empty
+;;
