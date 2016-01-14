73198f89bc3f69b459b585a5bb2b2b67c14e30df
Leandro Facchinetti <me@leafac.com>
2016-01-14 13:17:07 -0500

    Add `new_constraints' to log

--- src/whayrf-typechecker/whayrf_constraint_closure_fixpoint.ml
+++ src/whayrf-typechecker/whayrf_constraint_closure_fixpoint.ml
@@ -1,17 +1,28 @@
 open Batteries;;
+open Printf;;
 
 open Whayrf_ast;;
 open Whayrf_consistency;;
 open Whayrf_initial_alignment;;
 open Whayrf_notation;;
+open Whayrf_logger;;
 open Whayrf_pattern_subsumption;;
 open Whayrf_type_compatibility;;
 open Whayrf_types;;
 open Whayrf_types_pretty;;
 open Whayrf_utils;;
 
+let logger = make_logger "Whayrf_constraint_fixpoint";;
+
 (** Run a list of closure rules to a fixpoint. *)
 let rec closure_fixpoint rules constraint_set =
+  logger `trace
+    (sprintf
+       "`closure_fixpoint' called with `constraint_set' = `%s'."
+       (pretty_constraint_set constraint_set)
+    )
+  ;
+
   let new_constraints =
     List.fold_left
       (
@@ -23,6 +34,12 @@ let rec closure_fixpoint rules constraint_set =
       Constraint_set.empty
       rules
   in
+  logger `trace
+    (sprintf
+       "`closure_fixpoint' found `new_constraints' = `%s'."
+       (pretty_constraint_set new_constraints)
+    )
+  ;
   if new_constraints = Constraint_set.empty then
     Constraint_set.empty
   else
