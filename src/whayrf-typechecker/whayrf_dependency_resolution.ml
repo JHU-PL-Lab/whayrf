29e52e475248fb5a37d35ea1db9e3ddf03642884
Leandro Facchinetti <me@leafac.com>
2016-01-15 10:00:30 -0500

    Add CYCLE BREAKER

--- src/whayrf-typechecker/whayrf_dependency_resolution.ml
+++ src/whayrf-typechecker/whayrf_dependency_resolution.ml
@@ -12,6 +12,13 @@ let pretty_dependency_graph dependency_graph =
   "TODO"
 ;;
 
+(* TODO: Not implemented. *)
 let dependencies function_pattern_matching_case dependency_graph =
+  Function_pattern_matching_case_set.empty
+;;
+
+(* TODO: Not implemented. *)
+(** Inspired by BLOCKING and CYCLE BREAKER from the main paper submission. *)
+let function_pattern_matching_cases_participating_in_cycles dependency_graph =
   Function_pattern_matching_case_set.empty
 ;;
