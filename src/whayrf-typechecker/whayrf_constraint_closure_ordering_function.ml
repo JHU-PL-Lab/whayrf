d55b88fecca0475103875e22e04f4c7b15f57d77
Leandro Facchinetti <me@leafac.com>
2016-01-26 15:45:40 -0500

    Add PATTERN-MATCHING rule to Ordering Function

--- src/whayrf-typechecker/whayrf_constraint_closure_ordering_function.ml
+++ src/whayrf-typechecker/whayrf_constraint_closure_ordering_function.ml
@@ -15,6 +15,87 @@ open Whayrf_utils;;
 
 let logger = make_logger "Whayrf_constraint_closure_ordering_function";;
 
+(** PATTERN-MATCHING *)
+let close_by_pattern_matching constraint_set =
+  constraint_set
+  |> Constraint_set.enum
+  |> Enum.filter_map
+    (
+      fun tconstraint ->
+        match tconstraint with
+        | Type_variable_constraint (
+            subject_type_variable,
+            Function_pattern (
+                parameter_pattern,
+                return_pattern
+              )
+          ) ->
+          Some (
+            constraint_set
+            |> Constraint_set.enum
+            |> Enum.filter_map
+              (
+                fun tconstraint ->
+                  match tconstraint with
+                  | Lower_bound_constraint(
+                      Restricted_type_lower_bound (
+                        Restricted_type (
+                          Function_type_type (
+                            Function_type (
+                              parameter_type_variable,
+                              Constrained_type (
+                                return_type_variable,
+                                body_constraint_set
+                              )
+                            )
+                          ),
+                          _
+                        )
+                      ),
+                      other_subject_type_variable
+                    ) ->
+                    if subject_type_variable = other_subject_type_variable then
+                      Some (
+                        Enum.append
+                          (Constraint_set.enum body_constraint_set)
+                          (
+                            List.enum [
+                              Lower_bound_constraint (
+                                Restricted_type_lower_bound (
+                                  Restricted_type (
+                                    Unknown_type,
+                                    Type_restriction (
+                                      Positive_pattern_set (
+                                        Pattern_set.singleton parameter_pattern
+                                      ),
+                                      Negative_pattern_set (
+                                        Pattern_set.empty
+                                      )
+                                    )
+                                  )
+                                ),
+                                parameter_type_variable
+                              );
+                              Type_variable_constraint (
+                                return_type_variable,
+                                return_pattern
+                              )
+                            ]
+                          )
+                      )
+                    else
+                      None
+                  | _ -> None
+              )
+            |> Enum.concat
+          )
+        | _ -> None
+    )
+  |> Enum.concat
+  |> Constraint_set.of_enum
+  |> Constraint_set.union constraint_set
+;;
+
 (** FUNCTION PATTERN SIMULATED CALL *)
 let close_by_function_pattern_simulated_call constraint_set =
   constraint_set
@@ -276,6 +357,7 @@ let ordering_function_closure constraint_set =
   closure_fixpoint
     [
       (* The order is irrelevant for the correctness of the program. *)
+      close_by_pattern_matching;
       close_by_function_pattern_simulated_call;
       close_by_function_simulated_success;
       close_by_function_simulated_failure
